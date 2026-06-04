9662824e1340ed58e5919fb125bb3fff55003e37
Andrey Moura <andreymds88@gmail.com>
2026-06-04 19:45:05 -0300

    Better error message

--- src/error.cpp
+++ src/error.cpp
@@ -4,15 +4,16 @@
 
 void lavi::lang::error::internal(std::string_view message)
 {
-    std::cerr
-        << "ANDY LANGUAGE INTERNAL ERROR!!!" << std::endl
-        << std::endl
-        << message << std::endl << std::endl
-        << "You have encountered an internal error in the Lavi Language." << std::endl
-        << "This is likely a bug in the language itself." << std::endl
-        << "Please report this issue to the Lavi developers at" << std::endl
-        << "https://github.com/Moonslate/lavi/issues"
-        << std::endl;
+std::cerr
+    << "\n"
+    << "LAVI LANGUAGE INTERNAL ERROR\n"
+    << "============================\n\n"
+    << message << "\n\n"
+    << "An internal error occurred while executing the program.\n"
+    << "This indicates a bug in the Lavi Language implementation.\n\n"
+    << "If possible, please create a minimal reproducible example\n"
+    << "that triggers this error and report it at:\n\n"
+    << "https://github.com/Moonslate/lavi/issues\n\n";
 
     std::abort();
 }
